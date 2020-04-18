1ec73f7e4624f2daa0971b3d61738ab85cb9d7ea
Bentzy <ben.roznboim@gmail.com>
2020-04-18 21:17:56 +0300

    .

--- src/service/axiosInstance.js
+++ src/service/axiosInstance.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const axiosInstance = axios.create({
-  //baseURL: "http://game.bengo.io:8000/",
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: "http://game.bengo.io:8000/",
   timeout: 60000,
   headers: {
     "Content-Type": "application/json"
